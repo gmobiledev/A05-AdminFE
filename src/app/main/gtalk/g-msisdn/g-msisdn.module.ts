de0138f30fd10f7e90ac80877f9292eb332a5dc5
dangnh <dangnh27@gmail.com>
2023-10-05 15:38:26 +0700

    fix

--- src/app/main/gtalk/g-msisdn/g-msisdn.module.ts
+++ src/app/main/gtalk/g-msisdn/g-msisdn.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { GMsisdnComponent } from './g-msisdn.component';
 
 
 @NgModule({
   declarations: [
-    
+    GMsisdnComponent
   ],
   imports: [
     CommonModule
